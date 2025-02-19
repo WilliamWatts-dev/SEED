f036c505ca503cd59eb7bb4d206024b2c0aae075
jschultz2 <jschultz2@mines.edu>
2025-02-19 10:46:41 -0700

    Update Mini_Project.ino

--- Mini_Project/Mini_Project.ino
+++ Mini_Project/Mini_Project.ino
@@ -1,4 +1,4 @@
-// Arduino pin connections: 
+// Pin connections:
 #include <Encoder.h> // Use the Encoder library for handling encoders
 #include <Wire.h> // Include the Wire library for I2C
 
@@ -68,6 +68,8 @@ unsigned int PWM2 = 0;
 // Quadrant value inputted from raspberry pi
 int quadrant = 0;
 
+
+
 void setup() {
     // Set motor pins as outputs
     pinMode(motor1PWM, OUTPUT);
@@ -81,10 +83,10 @@ void setup() {
 
     // Initialize Serial communication for debugging
     Serial.begin(115200);
-    Serial.println("Time(s)\tVoltage1(V)\tVelocity1(rad/s)\tVoltage2(V)\tVelocity2(rad/s)"); // Column headers
+    Serial.println("Time(s)\tVoltage1(V)\tVelocity1(rad/s)\tVoltage2(V)\tVelocity2(rad/s)\tQuadrant"); // Column headers
 
-    // Join I2C bus as slave with address 20
-    Wire.begin(0x20);
+    // Join I2C bus as slave with address 8
+    Wire.begin(8);
 
     // Initialize timing variables
     last_time_ms = millis();
@@ -94,39 +96,30 @@ void setup() {
 void loop() {
     // Get current time
     current_time = (float)(millis() - start_time_ms) / 1000.0;
-
-    if (Wire.available()) { // loop through all but the last
-      quadrant = Wire.read(); // receive pi values as uint8_t values
+    while (Wire.available()) { // loop through all but the last
+     quadrant = Wire.read(); // receive pi values as uint8_t values
+      }
+    if (current_time <= 5.0) {
+        desiredPos1 = pos_ZERO;
+        desiredPos2 = pos_ZERO; // Starting position
+    }
+    if (current_time > 5.0) {
+        switch (quadrant) {
+        case 0: // Northeast or Default
+          desiredPos1 = pos_ZERO; // 0
+          desiredPos2 = pos_ZERO; // 0
+        case 1: // Northwest
+          desiredPos1 = pos_ZERO; // 0
+          desiredPos2 = pos_ONE; // 1
+        case 2: // Southeast
+          desiredPos1 = pos_ONE; // 1
+          desiredPos2 = pos_ZERO; // 0
+        case 3: // Southwest
+          desiredPos1 = pos_ONE; // 1
+          desiredPos2 = pos_ONE; // 1
+      }
     }
 
-    // Switch statement for recieving CV information.
-    switch (quadrant) {
-      case 0: // Northeast or Default
-        desiredPos1 = pos_ZERO; // 0
-        desiredPos2 = pos_ZERO; // 0
-      case 1: // Northwest
-        desiredPos1 = pos_ZERO; // 0
-        desiredPos2 = pos_ONE; // 1
-      case 2: // Southeast
-        desiredPos1 = pos_ONE; // 1
-        desiredPos2 = pos_ZERO; // 0
-      case 3: // Southwest
-        desiredPos1 = pos_ONE; // 1
-        desiredPos2 = pos_ONE; // 1
-    }
-/* code for deugging desired position
-    // Set desired positions based on time (0 or 1 for each motor)
-    if (current_time <= 1.0) {
-        desiredPos1 = desiredPos2 = pos_ZERO; // Starting position
-    }
-    else if (current_time <= 6.0 ) {
-        desiredPos1 = pos_ZERO; // 1
-        desiredPos2 = pos_ONE; // 1
-    }
-    else {
-        desiredPos1 = desiredPos2 = pos_ZERO; // Back to starting position
-    }
-*/
     // Read motor position (in counts) and convert to radians
     long pos1_counts = motor1Encoder.read();
     float pos1_rad = 2 * PI * (float)pos1_counts / (countsPerRevolution * 4);
@@ -192,7 +185,9 @@ void loop() {
     Serial.print("\t");
     Serial.print(appliedVoltage2, 2);
     Serial.print("\t");
-    Serial.println(velocity2, 4);
+    Serial.print(velocity2, 4);
+    Serial.print("\t");
+    Serial.println(quadrant);
 
     // Ensure loop runs at the correct sample rate
     while (millis() < last_time_ms + desired_Ts * 1000) {
