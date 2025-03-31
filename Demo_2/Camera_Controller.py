7bc8a125376fc10fe767d4819409196834314176
mkelly8 <mkelly1@mines.edu>
2025-03-31 12:34:26 -0600

    Update Camera_Controller.py
    Added more initialization and global vars (angle, yaw, state, distance). Also moved camera code to its own thread

--- Demo_2/Camera_Controller.py
+++ Demo_2/Camera_Controller.py
@@ -7,9 +7,19 @@
 #
 # Extra Considerations:
 # Use moving averages
-# Calculate yaw to determine turn angle
-
-# Send blocks as (dist, angle)
+# Calculate yaw to use for alignment purposes
+# Calcualte the camera angle for movement purposes
+# Yaw should be 0 degrees before the camera angle is 0 degrees
+# Use state machine to determine which angle needs alignment
+# state 0 - initilzing
+# state 1 - marker not detected
+# state 2 - marker detected, reporting yaw
+# state 3 - marker detected, reproting global angle
+# state 4 - marker too  close, reporting turn angle
+# Send blocks as (dist, angle[,state,time??])
+# TODO I2C thread with simple state machine for decisions
+# Use with lock: and global flags to send data over i2c with threading
+# 
 
 import cv2
 from cv2 import aruco
@@ -18,7 +28,8 @@ from smbus2 import SMBus
 import board
 from time import sleep
 import logging
-
+import threading
+from pathlib import Path
 
 # GLOBAL VARS
 HEIGHT = 480
@@ -26,6 +37,17 @@ WIDTH = 640
 ARD_ADDR = 0x08
 #LCD_ADDR = 0x20
 offset = 0
+yaw_angle = 0
+camera_angle = 0
+distance = 0 
+
+lock = threading.Lock()
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(threadName)s - %(message)s'
+)
 
 # Initialize SMBus library with I2C bus 1
 bus = SMBUS(1)
@@ -34,52 +56,61 @@ bus = SMBUS(1)
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 parameters = aruco.DetectorParameters()
 
-# Initialize camera
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-print("Press 'q' in any window to quit")
+# Get local path
+script_directory = Path(__file__).resolve().parent
+calibration_directory = script_directory / "camera_calibration.npz"
 
-try:
-    while True:
-        
-        # Break loop with 'q' key
-        k = cv2.waitKey(1) & 0xFF
-        if k == ord('q'):
-            break
+# Get camera matrix from camera_calibration.npz
+with np.load(calibration_directory) as data:
+    camera_matrix = data['camera_matrix.npy']
+    dist_coeffs = data['dist_coeffs.npy']
+    
+# Thread 1: Camera Processing thread
+# Capture frame and update yaw_angle, camera_angle, distance, state(,turn_angle & flags)
+def camera_thread():
+    global yaw_angle, camera_angle, distance, state
 
-        # Capture frame
-        ret, frame = cap.read()
-        if not ret:
-            logging.error("Failed to grab frame")
-            break
-        
-        # Undistort frame
-        dst = cv2.undistort(frame, camera_matrix, dist_coeffs)
+    # Initialize camera
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+    print("Press 'q' in any window to quit")
 
-        # Convert to grayscale
-        gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    try: ## TODO move code to seperate threads (thread for i2c etc.)
+        while True:
+            
+            # Break loop with 'q' key
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord('q'):
+                break
+    
+            # Capture frame
+            ret, frame = cap.read()
+            if not ret:
+                logging.error("Failed to grab frame")
+                break
 
-        # Detect markers
-        corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-     
-        # Catch if marker not detected
-        if ids is None or corners is None:
-            cv2.imshow('Aruco Detection', dst)
-            continue
+            # Undistort frame
+            dst = cv2.undistort(frame, camera_matrix, dist_coeffs)
+    
+            # Convert to grayscale
+            gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    
+            # Detect markers
+            corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
-        # Determine index of closest marker
-        closest_marker_idx = np.argmin(tvecs[:, 0, 2])
+            # Catch if marker not detected
+            if ids is None or corners is None:
+                cv2.imshow('Aruco Detection', dst) # dont need imshow?
+                continue
 
-        # Store closest marker into rvec & tvec
-        rvec = rvecs[closest_marker_idx]
-        tvec = tvecs[closest_marker_idx][0] #[x,y,z]
-
-
-
-
-
-
+            # Determine index of closest marker
+            closest_marker_idx = np.argmin(tvecs[:, 0, 2]) # Replace : with mask filtering out entries that are too close
+    
+            # Store closest marker into rvec & tvec
+            rvec = rvecs[closest_marker_idx]
+            tvec = tvecs[closest_marker_idx][0] #[x,y,z]
+    
 
 
 
